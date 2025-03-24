109839fc2255559fa51cd8cef4f247698ac5c75c
Alp Salcıoğlu <alpsalcioglu10@gmail.com>
2025-03-24 14:29:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require("express-ejs-layouts");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+
+//Templating Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use('/', require('./server/routes/main'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
